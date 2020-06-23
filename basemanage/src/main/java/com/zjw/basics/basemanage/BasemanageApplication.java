f275c779d6a5e15da5b977ff69a80a27691a7bae
ibay-nawi <ibay_nawi@sina.com>
2020-06-23 14:47:19 +0800

    初始化项目

---
+++ basemanage/src/main/java/com/zjw/basics/basemanage/BasemanageApplication.java
@@ -0,0 +1,13 @@
+package com.zjw.basics.basemanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasemanageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasemanageApplication.class, args);
+	}
+
+}
---
+++ basemanage/src/test/java/com/zjw/basics/basemanage/BasemanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zjw.basics.basemanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasemanageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
